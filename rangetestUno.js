ab8b3fce55b8a640dc4d71448a5e906287382581
Justin Woodjac <jwoodjack@gmail.com>
2015-01-29 23:57:04 -0800

    test options

--- rangetestUno.js
+++ rangetestUno.js
@@ -8,7 +8,13 @@ var usonic = require('r-pi-usonic');
 
 var sensor = usonic.sensor(24, 23, 450);
 
+console.log('welcome')
+var distance = sensor();
+console.log(distance)
 
+
+/*
 setInterval(function(){
   console.log(sensor())
 }, 1000)
+*/
